0e388d9907174a5ed83650cd0fdf9bc0cd68ab98
Felipe Passos <felipekaires@hotmail.com>
2021-02-01 14:54:14 -0300

    .

--- juniper_codegen/src/common/parse/mod.rs
+++ juniper_codegen/src/common/parse/mod.rs
@@ -196,7 +196,7 @@ impl TypeExt for syn::Type {
             | T::Macro(_)
             | T::Never(_)
             | T::Verbatim(_)
-            | T::__TestExhaustive => {}
+            | T::__TestExhaustive(_) => {}
         }
     }
 }
